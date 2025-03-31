dca1f285a9bb4bced8242d951897298281362301
Pragadeesh0701 <pragadeeshbr0707@gmail.com>
2025-03-31 11:55:29 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "ecr_repo_url" {}
-variable "db_name" {}
-variable "db_user" {}
-variable "db_password" {}
-variable "s3_bucket_name" {}
