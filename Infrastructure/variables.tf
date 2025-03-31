6f8292d02c44c40c0cb44181ee08e6d042542108
Pragadeesh0701 <pragadeeshbr0707@gmail.com>
2025-03-31 12:00:00 +0530

    infra

---
+++ Infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecr_repo_url" {}
+variable "db_name" {}
+variable "db_user" {}
+variable "db_password" {}
+variable "s3_bucket_name" {}
